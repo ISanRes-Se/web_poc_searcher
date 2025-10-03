105ced4cd8c9d59189636e95ed0490cf08e57ec4
ISanRes-Se <ivan.sanchez.resina@seidor.com>
2025-10-03 10:52:39 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -42,10 +42,10 @@ function App() {
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="text-center mb-8">
           <h2 className="text-3xl font-bold text-gray-900 mb-2">
-            Buscador Inteligente
+            Busca en Egoitza Araba
           </h2>
           <p className="text-gray-600 max-w-2xl mx-auto">
-            Descubre información en todo el contenido indexado
+            Descubre información en todo el contenido indexado de la Diputación Foral de Álava
           </p>
         </div>
 
